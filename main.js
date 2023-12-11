eb8a5d9a47dc61135f7e323228ee114b43ea8825
Omar A. Elsayed <Omar.Elsayed@students.jmcss.org>
2023-12-11 10:05:28 -0600

    a

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
-
 var faq = document.getElementsByClassName("faq-page");
 var i;
 
 for (i = 0; i < faq.length; i++) {
-    faq[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
+  faq[i].addEventListener("click", function () {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
 
-        /* Toggle between hiding and showing the active panel */
-        var body = this.nextElementSibling;
-        if (body.style.display === "block") {
-            body.style.display = "none";
-        } else {
-            body.style.display = "block";
-        }
-    });
+    /* Toggle between hiding and showing the active panel */
+    var body = this.nextElementSibling;
+    if (body.style.display === "block") {
+      body.style.display = "none";
+    } else {
+      body.style.display = "block";
+    }
+  });
 }
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function(){
-      document.body.classList.add('loaded');
-      document.querySelector('h1').style.color = '#222222';
-    }, 1000);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(function () {
+    document.body.classList.add('loaded');
+    document.querySelector('h1').style.color = '#222222';
+  }, 1000);
+});
 
 
-  let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction()
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -46,4 +47,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-   
--- order.js
+++ order.js
@@ -1,167 +1,166 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Simulate order progress
-    simulateOrderProgress();
-  });
-  
-  function simulateOrderProgress() {
-    const progressBar = document.getElementById("progressbar-2");
-  
-    // Simulate order processing
-    setTimeout(() => {
-      updateProgressBar(progressBar, 1);
-    }, 1000);
-  
-    // Simulate order shipping
-    setTimeout(() => {
-      updateProgressBar(progressBar, 2);
-    }, 2000);
-  
-    // Simulate order en route
-    setTimeout(() => {
-      updateProgressBar(progressBar, 3);
-    }, 3000);
-  
-    // Simulate order arrival
-    setTimeout(() => {
-      updateProgressBar(progressBar, 4);
-    }, 4000);
-  }
-  
-  function updateProgressBar(progressBar, step) {
-    const steps = progressBar.children;
-  
-    for (let i = 0; i < steps.length; i++) {
-      if (i < step) {
-        steps[i].classList.add("active");
-        steps[i].classList.remove("text-muted");
-      } else {
-        steps[i].classList.remove("active");
-        steps[i].classList.add("text-muted");
-      }
+  // Simulate order progress
+  simulateOrderProgress();
+});
+
+function simulateOrderProgress() {
+  const progressBar = document.getElementById("progressbar-2");
+
+  // Simulate order processing
+  setTimeout(() => {
+    updateProgressBar(progressBar, 1);
+  }, 1000);
+
+  // Simulate order shipping
+  setTimeout(() => {
+    updateProgressBar(progressBar, 2);
+  }, 2000);
+
+  // Simulate order en route
+  setTimeout(() => {
+    updateProgressBar(progressBar, 3);
+  }, 3000);
+
+  // Simulate order arrival
+  setTimeout(() => {
+    updateProgressBar(progressBar, 4);
+  }, 4000);
+}
+
+function updateProgressBar(progressBar, step) {
+  const steps = progressBar.children;
+
+  for (let i = 0; i < steps.length; i++) {
+    if (i < step) {
+      steps[i].classList.add("active");
+      steps[i].classList.remove("text-muted");
+    } else {
+      steps[i].classList.remove("active");
+      steps[i].classList.add("text-muted");
     }
   }
-  document.addEventListener("DOMContentLoaded", function () {
-    // Simulate order progress every hour
-    setInterval(simulateOrderProgress, 3600000); // 1 hour in milliseconds
-  });
-  
-  function simulateOrderProgress() {
-    const progressBar = document.getElementById("progressbar-2");
-  
-    // Simulate order processing
-    setTimeout(() => {
-      updateProgressBar(progressBar, 1);
-      updateExpectedArrivalDate();
-    }, 1000);
-  
-    // Simulate order shipping
-    setTimeout(() => {
-      updateProgressBar(progressBar, 2);
-    }, 2000);
-  
-    // Simulate order en route
-    setTimeout(() => {
-      updateProgressBar(progressBar, 3);
-    }, 3000);
-  
-    // Simulate order arrival
-    setTimeout(() => {
-      updateProgressBar(progressBar, 4);
-    }, 4000);
-  }
-  
-  function updateProgressBar(progressBar, step) {
-    const steps = progressBar.children;
-  
-    for (let i = 0; i < steps.length; i++) {
-      if (i < step) {
-        steps[i].classList.add("active");
-        steps[i].classList.remove("text-muted");
-      } else {
-        steps[i].classList.remove("active");
-        steps[i].classList.add("text-muted");
-      }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate order progress every hour
+  setInterval(simulateOrderProgress, 3600000); // 1 hour in milliseconds
+});
+
+function simulateOrderProgress() {
+  const progressBar = document.getElementById("progressbar-2");
+
+  // Simulate order processing
+  setTimeout(() => {
+    updateProgressBar(progressBar, 1);
+    updateExpectedArrivalDate();
+  }, 1000);
+
+  // Simulate order shipping
+  setTimeout(() => {
+    updateProgressBar(progressBar, 2);
+  }, 2000);
+
+  // Simulate order en route
+  setTimeout(() => {
+    updateProgressBar(progressBar, 3);
+  }, 3000);
+
+  // Simulate order arrival
+  setTimeout(() => {
+    updateProgressBar(progressBar, 4);
+  }, 4000);
+}
+
+function updateProgressBar(progressBar, step) {
+  const steps = progressBar.children;
+
+  for (let i = 0; i < steps.length; i++) {
+    if (i < step) {
+      steps[i].classList.add("active");
+      steps[i].classList.remove("text-muted");
+    } else {
+      steps[i].classList.remove("active");
+      steps[i].classList.add("text-muted");
     }
   }
-  
-  function updateExpectedArrivalDate() {
-    const expectedArrivalDateElement = document.querySelector(
-      ".text-end > p:nth-child(1) > span"
-    );
-  
-    // Update expected arrival date (e.g., add one day)
-    const currentDate = new Date(expectedArrivalDateElement.textContent);
-    currentDate.setDate(currentDate.getDate() + 1);
-  
-    // Format the date to "MM/DD/YY"
-    const formattedDate = currentDate.toLocaleDateString("en-US", {
-      month: "2-digit",
-      day: "2-digit",
-      year: "2-digit",
-    });
-  
-    expectedArrivalDateElement.textContent = formattedDate;
-  }
-  document.addEventListener("DOMContentLoaded", function () {
-    // Simulate order progress every 2 hours (7200000 milliseconds)
-    setInterval(simulateOrderProgress, 7200000);
+}
+
+function updateExpectedArrivalDate() {
+  const expectedArrivalDateElement = document.querySelector(
+    ".text-end > p:nth-child(1) > span"
+  );
+
+  // Update expected arrival date (e.g., add one day)
+  const currentDate = new Date(expectedArrivalDateElement.textContent);
+  currentDate.setDate(currentDate.getDate() + 1);
+
+  // Format the date to "MM/DD/YY"
+  const formattedDate = currentDate.toLocaleDateString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "2-digit",
   });
-  
-  function simulateOrderProgress() {
-    const progressBar = document.getElementById("progressbar-2");
-  
-    // Simulate order processing
-    setTimeout(() => {
-      updateProgressBar(progressBar, 1);
-      updateExpectedArrivalDate();
-    }, 3000);
-  
-    // Simulate order shipping
-    setTimeout(() => {
-      updateProgressBar(progressBar, 2);
-    }, 6000);
-  
-    // Simulate order en route
-    setTimeout(() => {
-      updateProgressBar(progressBar, 3);
-    }, 9000);
-  
-    // Simulate order arrival
-    setTimeout(() => {
-      updateProgressBar(progressBar, 4);
-    }, 12000);
-  }
-  
-  function updateProgressBar(progressBar, step) {
-    const steps = progressBar.children;
-  
-    for (let i = 0; i < steps.length; i++) {
-      if (i < step) {
-        steps[i].classList.add("active");
-        steps[i].classList.remove("text-muted");
-      } else {
-        steps[i].classList.remove("active");
-        steps[i].classList.add("text-muted");
-      }
+
+  expectedArrivalDateElement.textContent = formattedDate;
+}
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate order progress every 2 hours (7200000 milliseconds)
+  setInterval(simulateOrderProgress, 7200000);
+});
+
+function simulateOrderProgress() {
+  const progressBar = document.getElementById("progressbar-2");
+
+  // Simulate order processing
+  setTimeout(() => {
+    updateProgressBar(progressBar, 1);
+    updateExpectedArrivalDate();
+  }, 3000);
+
+  // Simulate order shipping
+  setTimeout(() => {
+    updateProgressBar(progressBar, 2);
+  }, 6000);
+
+  // Simulate order en route
+  setTimeout(() => {
+    updateProgressBar(progressBar, 3);
+  }, 9000);
+
+  // Simulate order arrival
+  setTimeout(() => {
+    updateProgressBar(progressBar, 4);
+  }, 12000);
+}
+
+function updateProgressBar(progressBar, step) {
+  const steps = progressBar.children;
+
+  for (let i = 0; i < steps.length; i++) {
+    if (i < step) {
+      steps[i].classList.add("active");
+      steps[i].classList.remove("text-muted");
+    } else {
+      steps[i].classList.remove("active");
+      steps[i].classList.add("text-muted");
     }
   }
-  
-  function updateExpectedArrivalDate() {
-    const expectedArrivalDateElement = document.querySelector(
-      ".text-end > p:nth-child(1) > span"
-    );
-  
-    // Update expected arrival date (e.g., add one day)
-    const currentDate = new Date(expectedArrivalDateElement.textContent);
-    currentDate.setDate(currentDate.getDate() + 1);
-  
-    // Format the date to "MM/DD/YY"
-    const formattedDate = currentDate.toLocaleDateString("en-US", {
-      month: "2-digit",
-      day: "2-digit",
-      year: "2-digit",
-    });
-  
-    expectedArrivalDateElement.textContent = formattedDate;
-  }
-  
+}
+
+function updateExpectedArrivalDate() {
+  const expectedArrivalDateElement = document.querySelector(
+    ".text-end > p:nth-child(1) > span"
+  );
+
+  // Update expected arrival date (e.g., add one day)
+  const currentDate = new Date(expectedArrivalDateElement.textContent);
+  currentDate.setDate(currentDate.getDate() + 1);
+
+  // Format the date to "MM/DD/YY"
+  const formattedDate = currentDate.toLocaleDateString("en-US", {
+    month: "2-digit",
+    day: "2-digit",
+    year: "2-digit",
+  });
+
+  expectedArrivalDateElement.textContent = formattedDate;
+}
